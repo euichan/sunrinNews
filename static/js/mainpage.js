706c845a4b9609eff6e2ce3db31241240a20c5b4
Chmod777 <canon0115@naver.com>
2015-10-28 00:27:15 +0900

    fb_extra 수정
    fb_extra 다시 코딩함(이시훈 개객기)
    승의가 fb_extra에 level다이브name 어떻게 보낼지 시훈이랑 상의하기

--- static/js/mainpage.js
+++ static/js/mainpage.js
@@ -1,3 +1,6 @@
+
+/* 우진이 짜던 코드
+
 function ClickedSchool(event){
     document.getElementsByClassName("category")[0].school_hl.style.borderBottomColor="#ff0000";
     document.getElementsByClassName("category")[1].club_hl.style.borderBottomColor="transparent";
@@ -6,4 +9,15 @@ function ClickedSchool(event){
 function ClickedClub(event){
     document.getElementsByClassName("category")[0].school_hl.style.borderBottomColor="transparent";
     document.getElementsByClassName("category")[1].club_hl.style.borderBottomColor="#ff0000";
+}
+*/
+
+window.onload = function(){
+    window.addEventListener('scroll', function () {
+        var contentsContainer=document.getElementById("all_contents_container");
+        var topBar=document.getElementById("topbar");
+        if(($(contentsContainer).css("margin-top").replace("px","")-topBar.offsetHeight)<window.scrollY){
+            window.scrollTo(0,($(contentsContainer).css("margin-top").replace("px","")-topBar.offsetHeight));
+        }
+    });
 }
