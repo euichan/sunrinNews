a64daa33c9cbbee3f4e37087fdc91d951e7c7dcf
blackDevelop <h3065@naver.com>
2015-10-31 21:58:02 +0900

    Revert "to seungie"
    This reverts commit b4d28b7f3f6e60be6e1ee482d666600b98123f49.

--- static/js/register.js
+++ static/js/register.js
@@ -3,10 +3,11 @@ function seungie(e) {
     if (e.id == "btn_left") {
         $.fn.fullpage.moveSlideLeft();
     }
-   
+    
     $.fn.fullpage.moveSlideRight();
 }
 var csrftoken;
+/*
 window.onload = function () {
     $.ajaxSetup({
         beforeSend: function (xhr, settings) {
@@ -18,7 +19,7 @@ window.onload = function () {
         }
     });
 }
-
+*/
 function upload_img() {
     $('#id_img').trigger('click');
 }
@@ -32,25 +33,29 @@ function change_img() {
 
 function slideHandler( anchorLink, index, slideAnchor, slideIndex){
     if(slideIndex==2)
-    {
+        {
             
-    }
+        }
 }
 
 function sendData() {
     var formData = new FormData($('form')[0]);
+    console.log(formData);
+   // for (var i = 0; i < $("input[name=file]")[0].files.length; i++) {
+        //첫번째 파일태그
+   //     formData.append("img", $("input[name=file]")[0].files[i]);
+  //  }
     
     formData.append('level',level); 
     formData.append('img',$("input[name=img]")[0].files[0]);
     formData.append('csrfmiddlewaretoken',csrftoken);
     $.ajax({
-        url: '/accounts/signup',
+        url: '/signup.php',
         processData: false,
         contentType: false,
         data: formData,
         type: 'POST',
         success: function (result) {
-            window.location = '/accounts/signupcomplete'
         }
     });
 }
