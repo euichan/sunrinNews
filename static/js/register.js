8429b9dc8c17580577fe04c215f8dd78bea3664b
blackDevelop <h3065@naver.com>
2015-11-01 02:48:22 +0900

    sibal
    sibal

--- static/js/register.js
+++ static/js/register.js
@@ -1,9 +1,10 @@
-var level=0;
+var level = 0;
+
 function seungie(e) {
     if (e.id == "btn_left") {
         $.fn.fullpage.moveSlideLeft();
     }
-    
+
     $.fn.fullpage.moveSlideRight();
 }
 var csrftoken;
@@ -25,42 +26,52 @@ function upload_img() {
 }
 
 function change_img() {
-    if($('#id_img')[0].value) 
-        $('#preview')[0].src = $('#preview')[0].src.replace('_btn.','_mold.');
-    else 
-        $('#preview')[0].src = $('#preview')[0].src.replace('_mold.','_btn.');
+    if ($('#id_img')[0].value)
+        $('#preview')[0].src = $('#preview')[0].src.replace('_btn.', '_mold.');
+    else
+        $('#preview')[0].src = $('#preview')[0].src.replace('_mold.', '_btn.');
 }
 
-function slideHandler( anchorLink, index, slideAnchor, slideIndex){
-    if(slideIndex==2)
-        {
-            
-        }
+function slideHandler(anchorLink, index, slideAnchor, slideIndex) {
+    if (slideIndex == 2) {
+
+    }
 }
 
 function sendData() {
     var formData = new FormData($('form')[0]);
     console.log(formData);
-   // for (var i = 0; i < $("input[name=file]")[0].files.length; i++) {
-        //첫번째 파일태그
-   //     formData.append("img", $("input[name=file]")[0].files[i]);
-  //  }
-    
-    formData.append('level',level); 
-    formData.append('img',$("input[name=img]")[0].files[0]);
-    formData.append('csrfmiddlewaretoken',csrftoken);
+    // for (var i = 0; i < $("input[name=file]")[0].files.length; i++) {
+    //첫번째 파일태그
+    //     formData.append("img", $("input[name=file]")[0].files[i]);
+    //  }
+
+    formData.append('level', level);
+    formData.append('img', $("input[name=img]")[0].files[0]);
+    formData.append('csrfmiddlewaretoken', csrftoken);
     $.ajax({
         url: '/signup.php',
         processData: false,
         contentType: false,
         data: formData,
         type: 'POST',
-        success: function (result) {
-        }
+        success: function (result) {}
     });
 }
 
 function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+
+function error(str) {
+    var error = document.getElementsByClassName("alert")[0];
+    error.style.top="25%";
+    error.innerHTML = "<strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;에러!</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"+str+"!";
+    $(error).fadeIn(200, function () {
+        setTimeout(function () {
+            $(error).fadeOut(500, function () {});
+        }, 1000);
+    });
 }
